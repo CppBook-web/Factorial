cdd41d61680cf466f6d8586441e205dba0f63cc5
Android <Android@DESKTOP-LCDANA0>
2024-02-29 12:38:48 +0300

    Добавьте файлы проекта.

---
+++ Factorial.cpp
@@ -0,0 +1,24 @@
+﻿// Factorial.cpp
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    int n = 0;
+    long factorial = 1.0;
+
+    std::cout << "Введите положительное целое число: ";
+    std::cin >> n;
+
+    if (n < 0)
+        std::cout << "Ошибка! Факториал не может быть "
+            << "отрицательным числом.";
+    else {
+        for (int i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        std::cout << "Факториал числа " << n << " = "
+            << factorial;
+    }
+
+    return 0;
+}
